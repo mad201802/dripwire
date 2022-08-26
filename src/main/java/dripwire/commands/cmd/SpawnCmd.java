5764c6196ce573d7c1d5f35ca0205323bd9d639a
mad201802 <md148@hdm-stuttgart.de>
2022-08-26 11:16:58 +0200

    working /spawn

--- src/main/java/dripwire/commands/cmd/SpawnCmd.java
+++ src/main/java/dripwire/commands/cmd/SpawnCmd.java
@@ -1,14 +1,19 @@
 package dripwire.commands.cmd;
 
+import net.kyori.adventure.text.Component;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class SpawnCmd implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
-        commandSender.sendMessage("Spawn command executed");
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        if(sender instanceof Player p) {
+            p.teleport(p.getWorld().getSpawnLocation());
+            p.sendMessage(Component.text("Du wurdest zum Spawn teleportiert."));
+        }
         return true;
     }
 }
